b2b338f42f8159ecf8d19d0d9794dd6c2008507e
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-19 11:49:20 +0300

    Update ConditionalFalsePrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        if (a == b){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
